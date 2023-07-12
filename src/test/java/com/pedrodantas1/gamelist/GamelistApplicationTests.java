f7283d5ed0e95ed7301950ccd5722362c5e88c24
Pedro Dantas <pedrood1232@gmail.com>
2023-07-12 08:00:16 -0300

    Project created

---
+++ src/test/java/com/pedrodantas1/gamelist/GamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrodantas1.gamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
