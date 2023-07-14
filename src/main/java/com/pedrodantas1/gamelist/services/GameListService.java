f95ed8d07afd5460b7bcf14a635121a1664bd3df
Pedro Dantas <pedrood1232@gmail.com>
2023-07-14 20:56:12 -0300

    Add move games on list

--- src/main/java/com/pedrodantas1/gamelist/services/GameListService.java
+++ src/main/java/com/pedrodantas1/gamelist/services/GameListService.java
@@ -8,13 +8,18 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.pedrodantas1.gamelist.dto.GameListDTO;
 import com.pedrodantas1.gamelist.entities.GameList;
+import com.pedrodantas1.gamelist.projections.GameMinProjection;
 import com.pedrodantas1.gamelist.repositories.GameListRepository;
+import com.pedrodantas1.gamelist.repositories.GameRepository;
 
 @Service
 public class GameListService {
 	
 	@Autowired
 	private GameListRepository gameListRepository;
+	
+	@Autowired
+	private GameRepository gameRepository;
 
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
@@ -22,4 +27,18 @@ public class GameListService {
 		return result.stream().map(x -> new GameListDTO(x)).toList();
 	}
 	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+		//Atualiza no banco
+		int minValue = Integer.min(sourceIndex, destinationIndex);
+		int maxValue = Integer.max(sourceIndex, destinationIndex);
+		
+		for (int i = minValue; i <= maxValue; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+	}
 }
